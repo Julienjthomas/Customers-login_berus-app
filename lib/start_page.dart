cf168f853dd002deddbe522cd5924aa2f4d8b0d6
Julien Joseph Thomas <julienjthomas@gmail.com>
2020-07-14 08:26:17 +0530

    Cart App UI

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -1,5 +1,6 @@
 import 'package:farmcart/cart.dart';
 import 'package:farmcart/sign_up.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 
@@ -100,32 +101,38 @@ class _MyHomePageState extends State<MyHomePage> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Transform.translate(
-                      offset: Offset(-80.0, -71.0),
-                      child: SvgPicture.string(
-                        _shapeSVG_a50c2f5e330841039c704249200df18e,
-                        allowDrawingOutsideViewBox: true,
-                      ),
-                    ),
+//                    Transform.translate(
+//                      offset: Offset(-80.0, -71.0),
+//                      child: SvgPicture.string(
+//                        _shapeSVG_a50c2f5e330841039c704249200df18e,
+//                        allowDrawingOutsideViewBox: true,
+//                      ),
+//                    ),
                     SizedBox(
-                      height: 155.0,
+                      height: 50.0,
+                    ),
+                  Text('Welcome to Beru\'s app',style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                    fontStyle: FontStyle.italic
+                  ), ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                    SizedBox(
+                      height: 145.0,
                       child: Image.asset(
                         "images/logo.png",
                         fit: BoxFit.contain,
                       ),
                     ),
-                    SizedBox(height: 45.0),
-                    emailField,
-                    SizedBox(height: 25.0),
-                    passwordField,
-                    SizedBox(
-                      height: 35.0,
-                    ),
-                    loginButton,
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                    forgotPassword,
+                    SizedBox(height: 30.0),
+                    GoogleFbButton(text: 'Sign in with Google',img:'google.png',),
+                    SizedBox(height: 30.0),
+                    GoogleFbButton(text: 'Sign in with Facebook',img:'facebook.png',),
+                    SizedBox(height: 30.0),
+
                     Column(
                       children: <Widget>[
                         Text(
@@ -140,9 +147,23 @@ class _MyHomePageState extends State<MyHomePage> {
                           'Sign in with',
                           style: TextStyle(color: Colors.green, fontSize: 20),
                         ),
+                        SizedBox(height: 20.0),
+                        emailField,
+                        SizedBox(height: 20.0),
+                        passwordField,
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        loginButton,
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    forgotPassword,
+
+
                       ],
                     ),
-                    _buildSocialBtnRow(),
+//                    _buildSocialBtnRow(),
                     signUp,
                   ],
                 ),
@@ -155,56 +176,48 @@ class _MyHomePageState extends State<MyHomePage> {
   static const _shapeSVG_a50c2f5e330841039c704249200df18e =
       '<svg viewBox="100.0 -20.0 50.0 100" ><defs><linearGradient id="gradient" x1="0.544051" y1="1.19412" x2="0.521159" y2="-0.219403"><stop offset="0.0" stop-color="#ff984c22"  /><stop offset="0.490928" stop-color="#ff356f23"  /><stop offset="1.0" stop-color="#ff43a047"  /></linearGradient></defs><path transform="translate(0.0, 24.0)" d="M -44 96 L -13 96 C -13 96 1.572830200195313 63.95188903808594 73 69 C 169 72 199 120 306 105 C 424 85 478 11 478 11 L 478 -95 L -44 -95 L -44 96 Z" fill="url(#gradient)" stroke="#707070" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
 
-  Widget _buildSocialBtn(Function onTap, AssetImage logo) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        height: 40.0,
-        width: 40.0,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black26,
-              offset: Offset(0, 2),
-              blurRadius: 6.0,
-            ),
-          ],
-          image: DecorationImage(
-            image: logo,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSocialBtnRow() {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 30.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          _buildSocialBtn(
-            () => print('Login with Facebook'),
-            AssetImage(
-              'images/facebook.png',
-            ),
-          ),
-          _buildSocialBtn(
-            () => print('Login with Google'),
-            AssetImage(
-              'images/google.png',
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 
   final kLabelStyle = TextStyle(
     color: Colors.teal,
     fontWeight: FontWeight.bold,
     fontFamily: 'OpenSans',
   );
+}
+
+class GoogleFbButton extends StatelessWidget {
+  GoogleFbButton({this.text,this.img});
+  final String text;
+  final String img;
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.white,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) {
+              return CartDisplay();
+            }),
+          );
+        },
+        child: Row(
+          children: <Widget>[
+            Expanded(child: Image(image: AssetImage('images/'+img),height: 15.0,)),
+            Padding(
+              padding: const EdgeInsets.only(right: 50.0),
+              child: Text(text,
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
