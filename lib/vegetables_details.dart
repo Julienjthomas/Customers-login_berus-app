5a4ad25a52e28555cf648bd910dea139c05af4e5
Julien Joseph Thomas <julienjthomas@gmail.com>
2020-07-27 00:36:11 +0530

    Cart App UI

--- lib/vegetables_details.dart
+++ lib/vegetables_details.dart
@@ -1,10 +1,13 @@
 //Generally if we click on a product from start product listing page ,Normally it goes to this page
 
 import 'package:farmcart/components/bottom_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class VegetableDetails extends StatelessWidget {
   final assetPath, vegPrice, vegName;
+
+  final ScrollController _scrollController=ScrollController();
 
   VegetableDetails({this.assetPath, this.vegName, this.vegPrice});
 
@@ -42,6 +45,7 @@ class VegetableDetails extends StatelessWidget {
         backgroundColor: Colors.white,
       ),
       body: ListView(
+        controller: _scrollController,
         children: <Widget>[
           SizedBox(
             height: 15.0,
@@ -140,7 +144,7 @@ class VegetableDetails extends StatelessWidget {
           Center(
             child: Container(
               width: MediaQuery.of(context).size.width - 50.0,
-              height: 50.0,
+              height: 40.0,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(25.0),
                 color: Colors.green,
@@ -157,16 +161,162 @@ class VegetableDetails extends StatelessWidget {
               ),
             ),
           ),
+          AspectRatio(
+            aspectRatio: 20.0,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              BeruFeatures(
+                path: 'images/contactless.jpg',
+              ),
+              BeruFeatures(
+                path: 'images/customer.jpg',
+              ),
+              BeruFeatures(
+                path: 'images/homedelivery.jpg',
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 20.0),
+            child: Container(
+              height: 1.0,
+              width: double.infinity,
+              color: Colors.grey,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Have a question?',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: Text(
+                    'Find answers in product info, Q&As, reviews',
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Type your question or keyword',
+                      prefixIcon: Icon(Icons.search),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+            child: Container(
+              height: 3.0,
+              width: double.infinity,
+              color: Colors.grey,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left:20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('From the Farmer',style: TextStyle(fontSize: 19.0,fontWeight: FontWeight.bold),),
+                Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Center(child: Image(image: AssetImage(assetPath),)),
+                ),
+                Text(
+                  'Very good fruits produced from organic farming.It is directly collected and distributed by Beru.Vegetables are good sources of minerals, especially calcium and iron, and vitamins, principally A and C. Nearly all vegetables are rich in dietary fibre and antioxidants. Vegetables are usually classified on the basis of the part of the plant that is used for food.',
+                  style: TextStyle(
+                      fontFamily: 'Varela',
+                      fontSize: 16.0,
+                      color: Colors.black),),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 20.0),
+            child: Container(
+              height: 1.0,
+              width: double.infinity,
+              color: Colors.grey,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left:20.0,right: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Customer Questions',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                AspectRatio(aspectRatio: 30.0),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Have a question? Search for answers',
+                    prefixIcon: Icon(Icons.search),
+                  ),
+                ),
+                AspectRatio(aspectRatio: 30.0),
+                Container(
+                  height: 200,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                    ),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.green,
         child: Icon(Icons.fastfood),
-//       backgroundColor: Color(0x),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomBar(),
+    );
+  }
+}
+
+class BeruFeatures extends StatelessWidget {
+  BeruFeatures({@required this.path});
+  final String path;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      width: 60.0,
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage(path), fit: BoxFit.contain),
+      ),
     );
   }
 }
